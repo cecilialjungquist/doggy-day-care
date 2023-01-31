3b4fc1238703726a9c1cf2e1fae5b732bf80a194
cecilialjungquist <111697322+cecilialjungquist@users.noreply.github.com>
2023-01-31 17:56:07 +0100

    Rendering out dogs from file done

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const ourDogsEl = document.getElementById('our-dogs');
+let dogSectionEl = document.querySelector('.dog-section');
+
 
 async function getDogs() {
     let dogs = await fetch('dogs.json');
@@ -6,11 +8,36 @@ async function getDogs() {
     console.log(dogs);
 
     dogs.forEach(dog => {
-        console.log(dog.name);
+
+        dogSectionEl.innerHTML += `
+            <article class="dog-card">
+                <aside id="${dog.chipNumber}">here</aside>
+                <img src="${dog.img}" alt="dog">
+                <ul>
+                    <li>name: ${dog.name}</li>
+                    <li>age: ${dog.age}</li>
+                    <li>sex: ${dog.sex}</li>
+                    <li>chipID: ${dog.chipNumber}</li>
+                    <li>owner: ${dog.owner.name} ${dog.owner.lastName}</li>
+                    <li>phone: ${dog.owner.phoneNumber}</li>
+                </ul>
+            </article>
+        `
+        if (!dog.present) {
+            document.getElementById(dog.chipNumber).style.display = 'none';
+        }
+
     });
-}
+};
 
 ourDogsEl.addEventListener('click', () => {
     document.querySelector('header').style.display = 'none';
+    document.querySelector('main').style.display = 'flex';
 
+    getDogs();
 });
+
+
+// om hunden inte är här, display = none
+// glöm inte take me back
+// hantera error?
